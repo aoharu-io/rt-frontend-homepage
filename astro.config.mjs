b756e75c94ee07d24d9fb12b3cd3cc77a83fb4b0
Takagi Tasuku <tasuren@outlook.jp>
2023-06-18 16:24:36 +0900

    Astroをi18nの設定までセットアップ。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+
+const DEFAULT_LAYOUT = '/src/layouts/Main.astro';
+function setDefaultLayout() {
+  return function (_, file) {
+    const {
+      frontmatter
+    } = file.data.astro;
+    if (!frontmatter.layout) frontmatter.layout = DEFAULT_LAYOUT;
+  };
+}
+;
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [setDefaultLayout]
+  },
+  experimental: {
+    assets: true
+  },
+  integrations: [mdx()]
+});
